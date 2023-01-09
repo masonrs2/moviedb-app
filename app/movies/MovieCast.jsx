cf0b4c6add29f2e051c00b0bdcf22fe184da2989
masonrs2 <masonsepulveda@gmail.com>
2023-01-08 23:08:39 -0600

    added cast to movie section

--- app/movies/MovieCast.jsx
+++ app/movies/MovieCast.jsx
@@ -18,7 +18,7 @@ const rightSlider = () => {
 async function fetchCredits(movieId) {
     const res = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=6259a38ecc880ab73db793a33df58313&language=en-US`)
     const data = await res.json();
-    console.log("movieId: ", data)
+    console.log("movieId: ", data.cast)
 
     return data.cast;
 }
@@ -26,7 +26,7 @@ async function fetchCredits(movieId) {
 const MovieCast =  ({ movie }) => {
     const credits = use(fetchCredits(movie.id))
     return (
-        <div className=" px-10 xl:px-32 pt-8 pb-4">
+        <div className=" px-10 xl:px-32 pt-8 pb-4 bg-white w-full">
            <div className="flex flex-col">
               <h1 className="text-black font-medium text-2xl mb-3">
                   Movie Cast
@@ -39,18 +39,18 @@ const MovieCast =  ({ movie }) => {
     
               <div id={'slider'} className="flex gap-4 relative w-full overflow-x-scroll scroll-smooth scrollbar-hide group ">
                   {
-                    credits.map((movie, index) => (
+                    credits.map((credit, index) => (
                       <div className="hover:bg-gray-300/30 hover:border border px-6 pt-6 active:bg-gray-300/50 hover:border-gray-500/30 cursor-pointer rounded-lg" key={index}>
-                        <Link href={`/movies/${movie.id}`}>
+                
                           <div className="flex flex-col rounded-lg w-40 group relative">
                               <img 
-                                src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} 
+                                src={`https://image.tmdb.org/t/p/original/${credit?.profile_path}`} 
                                 className="object-cover w-40 rounded-lg h-[220px] alt={item?.title} "
                                 />
-                            <h1 className="text-black font-semibold mt-3 ">{credits?.name}</h1>
+                            <h2 className="text-black font-semibold mt-3 w-full justify-center items-center flex ">{credit?.name}</h2>
+                            <h3 className="text-gray-600 text-xs font-semibold mt-3 w-full justify-center items-center flex ">{credit?.character}</h3>
                             
                           </div>
-                        </Link>
                       </div>
                     ))
                   }
